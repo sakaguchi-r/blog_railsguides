b04163db436be52a288216580d11b0579a7f964f
besaklish <besaklish@gmail.com>
2022-03-23 18:14:18 +0900

    feat: user can add articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
   root "articles#index"
 
-  get "/articles",      to: "articles#index"
-  get "/articles/:id",  to: "articles#show"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :articles
 end
