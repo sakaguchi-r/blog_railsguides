7217bb1bcc8832eed0dc981e922d9c0d45e5ab3d
besaklish <besaklish@gmail.com>
2022-03-28 12:12:11 +0900

    feat: do not show archived articles and comments

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,14 @@
 class Article < ApplicationRecord
-    has_many :comments
+  has_many :comments
 
-    validates :title, presence: true
-    validates :body, presence: true, length: { minimum: 10 }
+  validates :title, presence: true
+  validates :body, presence: true, length: { minimum: 10 }
+
+  VALID_STATUSES = ['public', 'private', 'archived']
+
+  validates :status, inclusion: { in: VALID_STATUSES }
+
+  def archived?
+    status == 'archived'
   end
-  
+end
